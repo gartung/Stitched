cms-sw/cmssw

Rename AutoLibraryLoader

DataFormats/FWLite/test/test.cppunit.cpp
@@ -8,7 +8,7 @@ Test program for edm::Ref use in ROOT.
 #include <string>
 #include <vector>
 #include <cppunit/extensions/HelperMacros.h>
-#include "FWCore/FWLite/interface/AutoLibraryLoader.h"
+#include "FWCore/FWLite/interface/Enable.h"
 #include "TFile.h"
 #include "TSystem.h"
 #include "DataFormats/TestObjects/interface/OtherThingCollection.h"
@@ -56,7 +56,7 @@ public:
   testRefInROOT() { }
   void setUp() {
     if(!sWasRun_) {
-      AutoLibraryLoader::enable();
+      fwlite::enable();
       sWasRun_ = true;
     }
     tmpdir = "tmp/";

FWCore/FWLite/bin/storageSize.cc
@@ -17,7 +17,7 @@
 #include "TBufferFile.h"
 
 // user include files
-#include "FWCore/FWLite/interface/AutoLibraryLoader.h"
+#include "FWCore/FWLite/interface/Enable.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
 
@@ -66,7 +66,7 @@ int main(int argc, char* argv[]) try
    
    std::string className(vm[kClassNameOpt].as<std::string>());
    
-   AutoLibraryLoader::enable();
+   fwlite::enable();
 
    TClass* cls = TClass::GetClass(className.c_str());
    if(0==cls) {

FWCore/FWLite/bin/storageSizeForBranch.cc
@@ -20,7 +20,7 @@
 #include "TTree.h"
 
 // user include files
-#include "FWCore/FWLite/interface/AutoLibraryLoader.h"
+#include "FWCore/FWLite/interface/Enable.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
 
@@ -108,7 +108,7 @@ int main(int argc, char* argv[]) try
    }
    
    
-   AutoLibraryLoader::enable();
+   fwlite::enable();
 
    TClass* cls = TClass::GetClass(branch->GetClassName());
    if(0==cls) {

gartung/Stitched

DataFormats/FWLite/test/test.cppunit.cpp
@@ -8,7 +8,7 @@ Test program for edm::Ref use in ROOT.
 #include <string>
 #include <vector>
 #include <cppunit/extensions/HelperMacros.h>
-#include "FWCore/FWLite/interface/AutoLibraryLoader.h"
+#include "FWCore/FWLite/interface/FWLiteEnabler.h"
 #include "TFile.h"
 #include "TSystem.h"
 #include "DataFormats/TestObjects/interface/OtherThingCollection.h"
@@ -56,7 +56,7 @@ public:
   testRefInROOT() { }
   void setUp() {
     if(!sWasRun_) {
-      AutoLibraryLoader::enable();
+      FWLiteEnabler::enable();
       sWasRun_ = true;
     }
     tmpdir = "tmp/";

FWCore/FWLite/bin/storageSize.cc
@@ -17,7 +17,7 @@
 #include "TBufferFile.h"
 
 // user include files
-#include "FWCore/FWLite/interface/AutoLibraryLoader.h"
+#include "FWCore/FWLite/interface/FWLiteEnabler.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
 
@@ -66,7 +66,7 @@ int main(int argc, char* argv[]) try
    
    std::string className(vm[kClassNameOpt].as<std::string>());
    
-   AutoLibraryLoader::enable();
+   FWLiteEnabler::enable();
 
    TClass* cls = TClass::GetClass(className.c_str());
    if(0==cls) {

FWCore/FWLite/bin/storageSizeForBranch.cc
@@ -20,7 +20,7 @@
 #include "TTree.h"
 
 // user include files
-#include "FWCore/FWLite/interface/AutoLibraryLoader.h"
+#include "FWCore/FWLite/interface/FWLiteEnabler.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
 
@@ -108,7 +108,7 @@ int main(int argc, char* argv[]) try
    }
    
    
-   AutoLibraryLoader::enable();
+   FWLiteEnabler::enable();
 
    TClass* cls = TClass::GetClass(branch->GetClassName());
    if(0==cls) {